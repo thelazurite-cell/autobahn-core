7698890bd124749da7b636fe9365b534346e791d
Dylan Eddies <me@dylaneddies.co.uk>
2022-04-15 21:52:18 +0100

    update the folder structure to be generated

--- src/framework/generator/generator-cli.ts
+++ src/framework/generator/generator-cli.ts
@@ -14,95 +14,96 @@ registerPrompt('checkbox-plus', require('inquirer-checkbox-plus-prompt'));
 registerPrompt('autocomplete', require('inquirer-autocomplete-prompt'));
 
 export class GeneratorCli {
-  private project: Project = new Project();
-  public static importRoot: string[] = ['autobahn-core', 'dist'];
+    private project: Project = new Project();
+    public static importRoot: string[] = ['autobahn-core', 'dist'];
 
-  public async run(): Promise<void> {
-    const projectFile = join(process.cwd(), 'config', 'project.json');
+    public async run(): Promise<void> {
+        const projectFile = join(process.cwd(), 'config', 'project.json');
 
-    if (!existsSync(projectFile)) {
-      await this.confirmGenerateProjectFile(projectFile);
-    }
+        if (!existsSync(projectFile)) {
+            await this.confirmGenerateProjectFile(projectFile);
+        }
 
-    const projectContents = JSON.parse(
-      readFileSync(projectFile, { encoding: 'utf8' })
-    );
-    Object.assign(this.project, projectContents);
-    const generateOptions = Object.keys(GenerateableTypes);
-    let generateType = null;
-    while (generateType != 'quit') {
-      generateType = await this.getGenerationType(generateOptions);
-      if (generateType != 'quit') {
-        const generator = FrameworkContainer.getTagged<IGenerator>(
-          FrameworkFunctionality.Generator,
-          FrameworkTags.generatorType,
-          generateType
+        const projectContents = JSON.parse(
+            readFileSync(projectFile, { encoding: 'utf8' })
         );
-        await generator.generate(this.project);
-      }
+        Object.assign(this.project, projectContents);
+        const generateOptions = Object.keys(GenerateableTypes);
+        let generateType = null;
+        while (generateType != 'quit') {
+            generateType = await this.getGenerationType(generateOptions);
+            if (generateType != 'quit') {
+                const generator = FrameworkContainer.getTagged<IGenerator>(
+                    FrameworkFunctionality.Generator,
+                    FrameworkTags.generatorType,
+                    generateType
+                );
+                await generator.generate(this.project);
+            }
+        }
     }
-  }
 
-  private async confirmGenerateProjectFile(projectFile: string): Promise<void> {
-    return new Promise((resolve, reject) => {
-      inquirer
-        .prompt([
-          {
-            type: 'confirm',
-            name: 'project',
-            message: `Couldn't find the project config, expected it to be in:\n ${projectFile}.\n\n Would you like to initialize a new project?`,
-            default: true,
-          },
-        ])
-        .then((answer) => {
-          if (answer.project) {
-            const configDirectory = join(process.cwd(), 'config');
-            const sourceDirectory = join(process.cwd(), 'src');
-            
-            if(!existsSync(configDirectory)) {
-                mkdirSync(configDirectory);
-            }
+    private async confirmGenerateProjectFile(projectFile: string): Promise<void> {
+        return new Promise((resolve, reject) => {
+            inquirer
+                .prompt([
+                    {
+                        type: 'confirm',
+                        name: 'project',
+                        message: `Couldn't find the project config, expected it to be in:\n ${projectFile}.\n\n Would you like to initialize a new project?`,
+                        default: true,
+                    },
+                ])
+                .then((answer) => {
+                    if (answer.project) {
+                        const folderStructure = [
+                            join(process.cwd(), 'config'),
+                            join(process.cwd(), 'src'),
+                            join(process.cwd(), 'src', 'products')
+                        ];
 
-            if(!existsSync(sourceDirectory)) {
-                mkdirSync(sourceDirectory);
-            }
-            
-            const project = new Project();
-            writeFileSync(
-              projectFile,
-              prettier.format(JSON.stringify(project), { parser: 'json' })
-            );
-          } else {
-            console.error('Cannot proceed without a package.json file');
-            process.exit(-1);
-          }
+                        for (const folder of folderStructure) {
+                            if (!existsSync(folder)) {
+                                mkdirSync(folder);
+                            }
+                        }
 
-          resolve();
-        })
-        .catch((e) => {
-          console.log(e);
-          reject(e);
+                        const project = new Project();
+                        writeFileSync(
+                            projectFile,
+                            prettier.format(JSON.stringify(project), { parser: 'json' })
+                        );
+                    } else {
+                        console.error('Cannot proceed without a package.json file');
+                        process.exit(-1);
+                    }
+
+                    resolve();
+                })
+                .catch((e) => {
+                    console.log(e);
+                    reject(e);
+                });
         });
-    });
-  }
+    }
 
-  private getGenerationType(generateOptions: string[]): Promise<string> {
-    return new Promise((resolve, reject) => {
-      prompt([
-        {
-          name: 'generateType',
-          message: 'What do you want to generate?',
-          type: 'list',
-          choices: [...generateOptions, new Separator(), 'quit'],
-          pageSize: 15,
-        },
-      ])
-        .then((answer) => {
-          const generateType = answer.generateType;
+    private getGenerationType(generateOptions: string[]): Promise<string> {
+        return new Promise((resolve, reject) => {
+            prompt([
+                {
+                    name: 'generateType',
+                    message: 'What do you want to generate?',
+                    type: 'list',
+                    choices: [...generateOptions, new Separator(), 'quit'],
+                    pageSize: 15,
+                },
+            ])
+                .then((answer) => {
+                    const generateType = answer.generateType;
 
-          resolve(generateType);
-        })
-        .catch((e) => reject(e));
-    });
-  }
+                    resolve(generateType);
+                })
+                .catch((e) => reject(e));
+        });
+    }
 }
